d9946ca94ed31f69edfda65cfa567c6401b4a794
Athar El-Haidary <48630732+asar17@users.noreply.github.com>
2023-06-11 16:28:41 +0200

    anatomy of a test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+test('render athar study react link',()=>{
+  render(<App/>);
+  const linkElement= screen.getByText(/athar study react/i);
   expect(linkElement).toBeInTheDocument();
 });
