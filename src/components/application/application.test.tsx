082d4c4abe76367e4de4eb2df8645e81148c34b0
Athar El-Haidary <48630732+asar17@users.noreply.github.com>
2023-06-12 20:37:25 +0200

    test app comp by getByLabelText with its option selector

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -17,21 +17,45 @@ describe('Application',()=>{
         })
         expect(sectionHeading).toBeInTheDocument()
 
+        //test nameElem by getByRole
         const nameElem= screen.getByRole('textbox',{
             name:"Name",
         })
         expect(nameElem).toBeInTheDocument()
+
+        //test nameElem by getByLabelText
+        const nameElem2= screen.getByLabelText('Name',{
+            selector:'input[type="text"]'
+        })
+        expect(nameElem2).toBeInTheDocument()
+
 
         const bioElem= screen.getByRole('textbox',{
             name:"Bio",
         })
         expect(bioElem).toBeInTheDocument()
 
+        //test jobLocationElem by getByRole
         const jobLocationElem= screen.getByRole('combobox')
         expect(jobLocationElem).toBeInTheDocument()
 
+        //test jobLocationElem by getByLabelText
+        const jobLocationElem2= screen.getByLabelText('Name',{
+            selector:'select'
+        })
+        expect(jobLocationElem2).toBeInTheDocument()
+
+
+        //test termsElemby getByRole
         const termsElem= screen.getByRole('checkbox')
         expect(termsElem).toBeInTheDocument()
+
+
+        //test termsElem by getByLabelText
+        const termsElem2= screen.getByLabelText('Name',{
+            selector:'input[type="checkbox"]'
+        })
+        expect(termsElem2).toBeInTheDocument()
 
         const submitButtonElem= screen.getByRole('button')
         expect(submitButtonElem).toBeInTheDocument()
--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -10,11 +10,13 @@ const Application = () =>{
                 <input type="text" id="name"/>
             </div>
             <div>
-                <label htmlFor="bio" >Bio</label>
+                <label htmlFor="bio">Bio</label>
                 <textarea id="bio" name="bio">write your bio</textarea>
             </div>
             <div>
-                <label htmlFor="job-location">Job Location</label>
+                {/* <label htmlFor="job-location">Job Location</label> */}
+                <label htmlFor="job-location">Name</label>
+
                 <select id="job-location">
                     <option value="">Select a Country</option>
                     <option value="US">United States</option>
@@ -25,7 +27,11 @@ const Application = () =>{
                 </select>
             </div>
             <div>
-                <input type="checkbox"/> I agree to the terms and conditions
+                <label>
+                  {/* <input type="checkbox" id="terms"/> I agree to the terms and conditions */}
+                  <input type="checkbox" id="terms"/> Name
+
+                </label>
             </div>
             <button>submit</button>
         </form>
